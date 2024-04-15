a8d9d38a22f6213005bfdb41e1d4f083b0ae1b08
windmi95 <windmi95@gmail.com>
2024-04-15 22:57:23 +0900

    xml_practice

---
+++ app/src/main/java/com/example/my_activity/thirdActivity.java
@@ -0,0 +1,30 @@
+package com.example.my_activity;
+
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class thirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_activity);
+
+        TextView textview;
+        textview = findViewById(R.id.textViewSpannable);
+
+
+
+
+
+    }
+
+
+
+
+
+}
