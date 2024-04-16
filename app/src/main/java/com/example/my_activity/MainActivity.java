12564fd9d4646d6c658c55bdf47237d0381a6ede
windmi95 <windmi95@gmail.com>
2024-04-16 23:22:06 +0900

    xml_practice0-0

--- app/src/main/java/com/example/my_activity/MainActivity.java
+++ app/src/main/java/com/example/my_activity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.my_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button move_btn = (Button) findViewById(R.id.kakaotalk_btn);
+        Button move_btn1 = (Button) findViewById(R.id.google_btn);
+        Button move_btn2 = (Button) findViewById(R.id.apple_btn);
+        Button move_btn3 = (Button) findViewById(R.id.facebook_btn);
+
+        move_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, thirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        move_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, thirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        move_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, thirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        move_btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, thirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/my_activity/thirdActivity.java
+++ app/src/main/java/com/example/my_activity/thirdActivity.java
@@ -1,10 +1,14 @@
 package com.example.my_activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class thirdActivity extends AppCompatActivity {
@@ -16,6 +20,19 @@ public class thirdActivity extends AppCompatActivity {
 
         TextView textview;
         textview = findViewById(R.id.textViewSpannable);
+        String content = textview.getText().toString();//textview text 가져오기
+        SpannableString spannableString = new SpannableString(content);// 객체 생성
+
+        String word = "제리아빠";
+        int start = content.indexOf(word);
+        int end = start + word.length();
+
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#028A0F")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textview.setText(spannableString);
+        // textview = "제리아빠" 글자색(초록색) 설정
+
+
 
 
 
