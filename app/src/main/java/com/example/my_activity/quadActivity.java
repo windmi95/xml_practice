6d72c7a1d7b4d6475e968bed2aeba41d5f5b1458
windmi95 <windmi95@gmail.com>
2024-04-20 14:31:40 +0900

    xml_practice0-3

---
+++ app/src/main/java/com/example/my_activity/quadActivity.java
@@ -0,0 +1,15 @@
+package com.example.my_activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class quadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quad_activity);
+
+    }
+}
